19dfce1a9c7f9056d876b2d169388a6caf6e492b
Navodit <navoditbharat@gmail.com>
2025-01-17 20:08:14 +0530

    fix

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -2,6 +2,6 @@ import * as turf from "@turf/turf";
 import { Polygon } from "../types";
 
 export const calculateArea = (polygon: Polygon): number => {
-  const turfPolygon = turf.polygon([polygon.coordinates]);
+  const turfPolygon = turf.polygon([polygon.coordinates as any]);
   return turf.area(turfPolygon);
 };
